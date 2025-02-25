6b5c71ef956c9aaf10c2414fd1f9e56b97010b32
KalilBENTAYEB <khalil.bentayeb95@gmail.com>
2025-02-25 23:25:03 +0000

    Create Auth functions

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|string|confirmed'
+        ]);
+
+        $user = User::create($validatedData);
+
+        $token = $user->createToken($request->name);
+
+        return 
+        [
+            '$user' => $user,
+            'token' => $token->plainTextToken
+        ];
+
+    }
+
+    public function login(Request $request)
+    {
+        $validatedData = $request->validate([
+            'email' => 'required|email|exists:users',
+            'password' => 'required'
+        ]);
+
+        $user = User::where('email', $validatedData['email'])->first();
+
+        if(!$user || !Hash::check($validatedData['password'], $user->password))
+        {
+            return response([
+                'message' => 'Invalid credentials'
+            ], 401);
+        }
+
+        $token = $user->createToken($user->name);
+
+        return 
+        [
+            '$user' => $user,
+            'token' => $token->plainTextToken
+        ];
+    }
+
+    public function logout(Request $request)
+    {   
+        $request->user()->tokens()->delete();
+        
+        return 
+        [
+            'message' => 'Logged out'
+        ];
+    }
+}
