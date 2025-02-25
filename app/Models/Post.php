d2d3cb8d8c195f0201eea016824055b44e61f61a
KalilBENTAYEB <khalil.bentayeb95@gmail.com>
2025-02-25 23:27:05 +0000

    Fill Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,4 +9,15 @@ class Post extends Model
 {
     /** @use HasFactory<\Database\Factories\PostFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'body',
+        'user_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
