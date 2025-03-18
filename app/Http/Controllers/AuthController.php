9c83889a5b4103871cb674ee9645ac94bd2d239e
KalilBENTAYEB <khalil.bentayeb95@gmail.com>
2025-03-18 17:31:56 +0000

    fix bug

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -48,7 +48,7 @@ class AuthController extends Controller
 
         return 
         [
-            '$user' => $user,
+            'user' => $user,
             'token' => $token->plainTextToken
         ];
     }
